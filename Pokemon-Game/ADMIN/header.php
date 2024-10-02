40c4042cdf748ec3fa2d26b49f4ad873ac4ddbab
French <152695796+FrenchPower7@users.noreply.github.com>
2024-10-02 17:48:24 +0200

    Add files via upload

--- Pokemon-Game/ADMIN/header.php
+++ Pokemon-Game/ADMIN/header.php
@@ -13,6 +13,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="profil.php">Profil</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="stats.php">Statistique</a>
+                </li>
             </ul>
         </div>
     </div>
